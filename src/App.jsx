a5d9255daa44635bdc5b47f95412725f2cdf0fc9
Hazar <hazarnamdarrr@gmail.com>
2024-10-24 16:54:00 +0300

    feature(core): The entire app skeleton and starter files are added to the repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+// React Libs
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Landing from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import NotFound from './pages/NotFound';
+import AppLayout from './pages/AppLayout';
+
 function App() {
   return (
-    <>
-      <p>Worldwise React App</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />} /> {/* Landing page */}
+        <Route path='/product' element={<Product />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/app' element={<AppLayout />} /> {/* Main page */}
+        <Route path='*' element={<NotFound />} />
+        {/* Any URL that's not found above will be directed to the page above */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppLayout(props) {
+  return (
+    <div>
+      <p>APP</p>
+    </div>
+  );
+}
+
+export default AppLayout;
